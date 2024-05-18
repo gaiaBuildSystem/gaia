d92aa0f7dd8c1f4cc329eeb0bdd28e360e178fcc
Matheus Castello <matheus@castello.eng.br>
2024-05-17 22:09:29 -0300

    apt: add apt completion script
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-common/apt/deploy.ts
@@ -0,0 +1,45 @@
+#!/opt/bun/bin/bun
+
+import PATH from "path"
+import FS from "fs"
+import logger from "node-color-log"
+import { execSync } from "child_process"
+import { Recipe } from "../../../scripts/bitcook/parse"
+import { getAssetPath } from "../../../scripts/bitcook/utils/getAssetPath"
+
+// run update in the chroot
+logger.info("deploy configured apt completion script ...")
+
+const ARCH = process.env.ARCH as string
+const MACHINE = process.env.MACHINE as string
+const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
+const BUILD_PATH = process.env.BUILD_PATH as string
+const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
+const DISTRO_MINOR = process.env.DISTRO_MINOR as string
+const DISTRO_PATCH = process.env.DISTRO_PATCH as string
+const USER_PASSWD = process.env.USER_PASSWD as string
+
+// get the recipe metadata
+const META = JSON.parse(process.env.META as string) as Recipe
+
+// get the actual script path, not the process.cwd
+const _path = PATH.dirname(process.argv[1])
+const _paths = META.paths.toString()
+const _getAssetPath = (_filePath) => getAssetPath(_filePath, _paths)
+
+const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
+const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
+process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
+process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
+
+// copy the fstab file to the rootfs
+execSync(
+    `echo ${USER_PASSWD} | sudo -E -S ` +
+    `cp ${_getAssetPath(`apt_completion`)} ${IMAGE_MNT_ROOT}/usr/share/bash-completion/completions/apt`,
+    {
+        shell: "/bin/bash",
+        stdio: "inherit",
+        encoding: "utf-8",
+        env: process.env
+    })
+logger.success("ok, apt completion is ok")
