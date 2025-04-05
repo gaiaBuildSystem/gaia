a939caba1f839b79def9b680b39e56de2a1ee628
Matheus Castello <matheus@castello.eng.br>
2025-04-05 02:53:44 -0300

    cookbook: simple-splash: Use hostAsContainer
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/simple-splash/deployInit.sh
+++ cookbook/recipes-common/simple-splash/deployInit.sh
@@ -4,22 +4,20 @@ set -e
 
 _path=$(dirname "$0")
 
-# go to the target rootfs because we need the binaries in the right architecture
-sudo -E chroot $IMAGE_MNT_ROOT /bin/bash -c "
-    cd / &&
-    pip3 install --break-system-packages setuptools &&
-    pip3 install --break-system-packages staticx &&
-    staticx /usr/bin/fbset fbset &&
-    staticx /usr/bin/fbi fbi
-"
+
+cd /
+staticx /usr/bin/fbset fbset
+staticx /usr/bin/fbi fbi
+
 
 # copy the static binaries to the initramfs folder
-sudo -E mv $IMAGE_MNT_ROOT/fbset $INITRAMFS_PATH/bin/fbset
-sudo -E mv $IMAGE_MNT_ROOT/fbi $INITRAMFS_PATH/bin/fbi
+mv /fbset $INITRAMFS_PATH/bin/fbset
+mv /fbi $INITRAMFS_PATH/bin/fbi
 
 # fbi need fonts
-sudo -E mkdir -p $INITRAMFS_PATH/usr/share/fonts
-sudo -E cp -r $IMAGE_MNT_ROOT/usr/share/fonts/ $INITRAMFS_PATH/usr/share/fonts/
+mkdir -p $INITRAMFS_PATH/usr/share/fonts
+
+cp -r /usr/share/fonts/ $INITRAMFS_PATH/usr/share/fonts/
 
 # deploy the image
 # check if the SIMPLE_SPLASH_PATH is set
@@ -28,14 +26,7 @@ if [ -z "$SIMPLE_SPLASH_PATH" ]; then
     SIMPLE_SPLASH_PATH=$_path/deimos.png
 fi
 
-sudo -E cp $SIMPLE_SPLASH_PATH $INITRAMFS_PATH/splash.png
+cp $SIMPLE_SPLASH_PATH $INITRAMFS_PATH/splash.png
 
 # deploy the script
-sudo -E cp $_path/busybox/00-splash.sh $INITRAMFS_PATH/scripts/00-splash.sh
-
-# clean the target rootfs
-sudo -E chroot $IMAGE_MNT_ROOT /bin/bash -c "
-    pip3 uninstall --break-system-packages -y setuptools &&
-    pip3 uninstall --break-system-packages -y staticx &&
-    apt-get purge -y python3-pip python3
-"
+cp $_path/busybox/00-splash.sh $INITRAMFS_PATH/scripts/00-splash.sh
