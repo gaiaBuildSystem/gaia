fdfa4b7c69abcc50cb6fb1fb70322706301316c8
Matheus Castello <matheus@castello.eng.br>
2025-06-22 17:54:00 -0300

    WIP: Add EFI payload support for x86_64
    We are trying to unify the programs used during the boot process
    of the x86 and arm, so we need to try run u-boot from EFI.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-bsp/u-boot/efi-payload.ts
+++ cookbook/recipes-bsp/u-boot/efi-payload.ts
@@ -26,24 +26,26 @@ const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
 process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
 process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
 
-// if the machine is the qemux86-64, we need to install the EFI payload
-// so, we skip this script
-if (MACHINE === "qemux86-64") {
-    logger.debug(`Skipping default u-boot deploy flow for ${MACHINE}`)
-    logger.debug(`${MACHINE} should use the efi-payload script instead`)
-    process.exit(0)
-}
 
 logger.info("installing u-boot image ...")
-execSync(
-    `sudo -k ` +
-    `cp ${BUILD_PATH}/tmp/${MACHINE}/u-boot/u-boot.bin ${IMAGE_MNT_BOOT}/`,
-    {
-        shell: "/bin/bash",
-        stdio: "inherit",
-        encoding: "utf-8",
-        env: process.env
-    })
+
+if (MACHINE === "qemux86-64") {
+    logger.warn("qemux86-64 with EFI u-boot is experimental!!")
+    logger.warn("YOU HAVE BEEN WARNED!!")
+
+    execSync(
+        `sudo -k ` +
+        `cp ${BUILD_PATH}/tmp/${MACHINE}/u-boot/u-boot*.efi ${IMAGE_MNT_BOOT}/EFI/BOOT/`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+} else {
+    throw new Error("Unsupported machine for EFI payload: " + MACHINE)
+}
+
 logger.success("u-boot image installed")
 
 logger.info("installing boot script ...")
