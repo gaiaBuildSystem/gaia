200ecacdd87ede7bc1da76a54d57d9bbbdd35afb
Matheus Castello <matheus@castello.eng.br>
2025-07-12 00:27:05 -0300

    bitcook: Add priority to the search of the assets in multiple paths
    The getAssetPath function was not considering the priority of the
    origins, so it was always returning the first found asset.
    Now, it will return the asset from the path with the highest priority.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/bitcook/utils/getAssetPath.ts
+++ scripts/bitcook/utils/getAssetPath.ts
@@ -3,19 +3,34 @@
 import logger from "node-color-log"
 import FS from "fs"
 
-export function getAssetPath(
+export function getAssetPath (
     assetFilePath: string,
-    paths: string
+    paths: [{
+        path: string,
+        priority: number
+    }]
 ): string {
-    const _paths = paths.split(",")
+    let _higher = {
+        path: "",
+        priority: -1
+    }
+    let _paths = paths.map((p) => p.path).join(", ")
 
-    for (const _path of _paths) {
-        const _assetPath = `${_path}/${assetFilePath}`
+    for (const _obj_path of paths) {
+        const _assetPath = `${_obj_path.path}/${assetFilePath}`
         if (FS.existsSync(_assetPath)) {
-            return _assetPath
+            if (
+                _obj_path.priority > _higher.priority
+            ) {
+                _higher = _obj_path
+            }
         }
     }
 
-    logger.error(`asset ${assetFilePath} not found in paths ${_paths}`)
-    throw new Error(`asset ${assetFilePath} not found in paths ${_paths}`)
+    if (_higher.priority === -1) {
+        logger.error(`asset ${assetFilePath} not found in paths ${_paths}`)
+        throw new Error(`asset ${assetFilePath} not found in paths ${_paths}`)
+    } else {
+        return `${_higher.path}/${assetFilePath}`
+    }
 }
