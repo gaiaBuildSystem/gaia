f2526c41e8be01af62872a48c34249cf8c2c2b9e
Matheus Castello <matheus@castello.eng.br>
2025-09-04 19:20:39 -0300

    simple-splash: 00-splash: Drop the use of fbset
    the fbset here is setting a wrong mode anyway, let the fbi autodetect
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/simple-splash/busybox/00-splash.sh
+++ cookbook/recipes-common/simple-splash/busybox/00-splash.sh
@@ -2,5 +2,11 @@
 
 echo "[initramfs] initializing simple splash screen..."
 
-/bin/fbset -t 39721 48 16 33 10 96 2
+mkdir -p /etc/fonts
+mkdir -p /var/cache/fontconfig
+chmod 777 /var/cache/fontconfig
+mkdir -p ~/.cache/fontconfig
+
+echo "[initramfs] modeset initializing..."
 /bin/fbi -T 1 -d /dev/fb0 --noverbose -autodown /splash.png >/dev/null 2>&1
+echo "[initramfs] splash initialized..."
