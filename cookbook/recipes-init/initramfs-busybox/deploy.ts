748630839a0ae31f0d0cca027189aebe007f9b4a
Matheus Castello <matheus@castello.eng.br>
2025-11-09 15:03:14 -0300

    recipes-init: Make the init be a template for other recipes
    This make more easy to overlays customizations over the base init.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-init/initramfs-busybox/deploy.ts
+++ cookbook/recipes-init/initramfs-busybox/deploy.ts
@@ -1,8 +1,11 @@
 #!/opt/bun/bin/bun
 
 import PATH from "path"
+import FS from "fs"
 import { execSync } from "child_process"
 import logger from "node-color-log"
+import { Recipe } from "../../../scripts/bitcook/parse"
+import { getAssetPath } from "../../../scripts/bitcook/utils/getAssetPath"
 
 // gaia need to previously set arhitecture and machine
 const ARCH = process.env.ARCH as string
@@ -10,9 +13,12 @@ const MACHINE = process.env.MACHINE as string
 const BUILD_PATH = process.env.BUILD_PATH as string
 const USER_PASSWD = process.env.USER_PASSWD as string
 const INITRAMFS_PATH = process.env.INITRAMFS_PATH as string
+const META = JSON.parse(process.env.META as string) as Recipe
 
 // get the actual script path, not the process.cwd
 const _path = PATH.dirname(process.argv[1])
+const _paths = META.paths
+const ORIGIN_PATH = _path
 process.env.ORIGIN_PATH = _path
 
 const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
@@ -20,15 +26,27 @@ const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
 process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
 process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
 
-execSync(
-    `sudo -k ` +
-    `cp ${_path}/init.sh ${INITRAMFS_PATH}/init`,
-    {
-        shell: "/bin/bash",
-        stdio: "inherit",
-        encoding: "utf-8",
-        env: process.env
-    })
+const _getAssetPath = (_filePath: string) => getAssetPath(_filePath, _paths)
+
+logger.info(`Parsing init.sh ${_path}/init.sh.templae ...`)
+
+// get it from the recipe origin or from here
+const _init_template_Path = _getAssetPath(`init.sh.template`)
+
+const _init_sh =
+    FS.readFileSync(
+        `${_init_template_Path}`,
+        "utf-8"
+    ).replace(
+        /{{MACHINE}}/g,
+        MACHINE
+    )
+
+// dump the parsed file
+FS.writeFileSync(
+    `${INITRAMFS_PATH}/init`,
+    _init_sh
+)
 
 execSync(
     `sudo -k ` +
