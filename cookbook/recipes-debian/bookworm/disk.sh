1cbfc69fbc179aa22416352ce345b8af85e307a7
Matheus Castello <matheus@castello.eng.br>
2026-01-11 20:08:43 -0300

    bookworm: disk: Add room for bootloader before first partition
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-debian/bookworm/disk.sh
+++ cookbook/recipes-debian/bookworm/disk.sh
@@ -21,7 +21,8 @@ dd if=/dev/zero of=$IMAGE_PATH \
     bs=1048576 count=${MAX_IMG_SIZE} status=progress
 
 # create the partition table
+# Start partitions at 8MB to leave space for bootloaders (e.g., i.MX platforms need ~2-4MB at 32KB)
 parted $IMAGE_PATH -s mktable msdos
-parted $IMAGE_PATH -s mkpart primary fat32 1 150 \
+parted $IMAGE_PATH -s mkpart primary fat32 8 158 \
     set 1 lba on align-check optimal 1 \
-    mkpart primary ext4 151 $(($MAX_IMG_SIZE - 151))
+    mkpart primary ext4 159 $(($MAX_IMG_SIZE - 151))
