1f5f2ffba366de971f41e9e76a464b13f0d9e32f
Matheus Castello <matheus@castello.eng.br>
2026-02-05 18:47:10 -0300

    recipes-debian: Add custom-feed recipes
    Is pretty normal to have custom feeds for Debian when working with
    embedded devices, so Gaia should provide an easy way to add these
    feeds to the system image. This commit adds a new recipe called
    "custom-feed" that allows you to specify custom Debian feeds in the
    machine's metadata, and it will automatically add the necessary files
    to the image during the build process.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ cookbook/recipes-debian/custom-feed/apply-custom-feed.ts
@@ -0,0 +1,174 @@
+#!/opt/bun/bin/bun
+
+import PATH from "path"
+import FS from "fs"
+import logger from "node-color-log"
+import { execSync } from "child_process"
+import { Recipe } from "../../../scripts/bitcook/parse"
+import { getAssetPath } from "../../../scripts/bitcook/utils/getAssetPath"
+
+// deploy the firmware blobs
+logger.info("Applying custom Debian feed ...")
+
+const ARCH = process.env.ARCH as string
+const MACHINE = process.env.MACHINE as string
+const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
+const BUILD_PATH = process.env.BUILD_PATH as string
+const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
+const DISTRO_MINOR = process.env.DISTRO_MINOR as string
+const DISTRO_PATCH = process.env.DISTRO_PATCH as string
+const USER_PASSWD = process.env.USER_PASSWD as string
+const META = JSON.parse(process.env.META as string) as Recipe
+
+// get the actual script path, not the process.cwd
+const _path = PATH.dirname(process.argv[1])
+const _paths = META.paths
+const _getAssetPath = (_filePath: string) => getAssetPath(_filePath, _paths)
+
+// in this step there is already a mounted image
+const IMAGE_MNT_BOOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/boot`
+const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
+process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
+process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
+
+
+interface DebianFeedConfig {
+    feeds?: [{
+        name: string
+        feedUrl: string
+        feedUri: string
+        suites: string
+        components: string
+        gpgKeyUrl: string
+        pinPriority: number
+    }]
+}
+
+const CUSTOM_FEED_DATA = META.customData[MACHINE] as DebianFeedConfig
+
+if (CUSTOM_FEED_DATA == undefined) {
+    logger.warn("No Custom Debian feed data found, skipping...")
+    process.exit(0)
+}
+
+const DEBIAN_FEEDS_PATH = PATH.join(
+    BUILD_PATH, "tmp", MACHINE, "deb-feeds"
+)
+
+if (
+    CUSTOM_FEED_DATA.feeds != undefined &&
+    CUSTOM_FEED_DATA.feeds.length > 0
+) {
+    FS.mkdirSync(DEBIAN_FEEDS_PATH, { recursive: true })
+
+    for (const feed of CUSTOM_FEED_DATA.feeds) {
+        // replace the custom-feeds.template file
+        const _feed_template_path = _getAssetPath(
+            "files/custom-feeds.template"
+        )
+
+        const _feed_output_path = PATH.join(
+            DEBIAN_FEEDS_PATH,
+            `${feed.name}`
+        )
+        let _feed_template = FS.readFileSync(
+            _feed_template_path, "utf-8"
+        )
+
+        _feed_template = _feed_template
+            .replace(/{{feedUrl}}/g, feed.feedUrl)
+            .replace(/{{pinPriority}}/g, feed.pinPriority.toString())
+        FS.writeFileSync(
+            _feed_output_path,
+            _feed_template,
+            "utf-8"
+        )
+
+        // copy it to the rootfs
+        execSync(
+            `sudo -k ` +
+            `cp ${_feed_output_path} ` +
+            `${IMAGE_MNT_ROOT}/etc/apt/preferences.d/${feed.name}`,
+            {
+                shell: "/bin/bash",
+                stdio: "inherit",
+                encoding: "utf-8",
+                env: process.env
+            })
+
+        // replace also the custom.sources.template
+        const _sources_template_path = _getAssetPath(
+            "files/custom-sources.template"
+        )
+        const _sources_output_path = PATH.join(
+            DEBIAN_FEEDS_PATH,
+            `${feed.name}.sources`
+        )
+
+        let _sources_template = FS.readFileSync(
+            _sources_template_path, "utf-8"
+        )
+
+        const _gpgKeyFileName =
+            PATH.basename(
+                feed.gpgKeyUrl
+            ).replace(
+                ".asc",
+                ".gpg"
+            )
+
+        _sources_template = _sources_template
+            .replace(/{{feedUri}}/g, feed.feedUri)
+            .replace(/{{suites}}/g, feed.suites)
+            .replace(/{{components}}/g, feed.components)
+            .replace(/{{gpgKeyUrl}}/g, _gpgKeyFileName)
+
+        FS.writeFileSync(
+            _sources_output_path,
+            _sources_template,
+            "utf-8"
+        )
+
+        // copy it to the rootfs
+        execSync(
+            `sudo -k ` +
+            `cp ${_sources_output_path} ` +
+            `${IMAGE_MNT_ROOT}/etc/apt/sources.list.d/${feed.name}.sources`,
+            {
+                shell: "/bin/bash",
+                stdio: "inherit",
+                encoding: "utf-8",
+                env: process.env
+            })
+
+        // apply the gpg key
+        execSync(
+            `sudo -k ` +
+            `chroot ${IMAGE_MNT_ROOT} /bin/bash -c "` +
+            `apt-get update && apt-get install -y curl gnupg` +
+            `"`,
+            {
+                shell: "/bin/bash",
+                stdio: "inherit",
+                encoding: "utf-8",
+                env: process.env
+            })
+
+        execSync(
+            `sudo -k ` +
+            `chroot ${IMAGE_MNT_ROOT} /bin/bash -c "` +
+            `curl -fsSL ${feed.gpgKeyUrl} | gpg --dearmor > /usr/share/keyrings/${_gpgKeyFileName}` +
+            `"`,
+            {
+                shell: "/bin/bash",
+                stdio: "inherit",
+                encoding: "utf-8",
+                env: process.env
+            })
+    }
+} else {
+    logger.warn("No Custom Debian feed data found, skipping...")
+    process.exit(0)
+}
+
+logger.success(`Custom Debian feed applied successfully!`)
