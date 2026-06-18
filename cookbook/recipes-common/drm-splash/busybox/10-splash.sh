b70b3b4f17b06553de26edbe0610a11dcc2494e0
Matheus Castello <matheus@castello.eng.br>
2026-06-18 20:43:56 -0300

    drm-splash: Get the dri card dinamically from the sys/class/drm
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-common/drm-splash/busybox/10-splash.sh
+++ cookbook/recipes-common/drm-splash/busybox/10-splash.sh
@@ -2,6 +2,9 @@
 
 echo "[initramfs] initializing drm splash screen..."
 
+# there are machines that we need to find the right card index
+CARD_INDEX=$(ls /sys/class/drm/ | grep -E '^card[0-9]+-' | sed 's/card\([0-9]*\)-.*/\1/' | sort -un | head -1)
+
 # this means drmi /splash.png /dev/dri/card0 do_not_scale
-/bin/drmi /splash.png 0 0 >/dev/null 2>&1 &
+/bin/drmi /splash.png $CARD_INDEX 0 >/dev/null 2>&1 &
 echo "[initramfs] splash initialized..."
