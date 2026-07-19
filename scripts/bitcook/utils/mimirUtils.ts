cd386f0c3cbfe548a08597855780f682fc12a3e0
Matheus Castello <matheus@castello.eng.br>
2026-07-18 23:03:48 -0300

    mimir: Improve the context for the build-<distro> folder generate
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/bitcook/utils/mimirUtils.ts
+++ scripts/bitcook/utils/mimirUtils.ts
@@ -89,7 +89,8 @@ mimir.setAdditionalContext(
     `USE THIS PATH AS BASE FOR THE COMMANDS SUGGESTIONS EVERYTHING WITH A PATH, AVOID cd WHENEVER POSSIBLE!\n\n` +
     `The project workdir has the following folders that are possible repos:\n` +
     `${_dirs.toArray().map(dir => `${_pwd}/${dir.name}`).join("\n")}\n\n` +
-    `Gaia build system generates folders for the distro build like ./build-<distro name>, these folders are not repos, but build output folders, avoid them when possible.\n\n`
+    `Gaia build system generates folders for the distro build like ./build-<distro name>, these folders are not repos, but build output folders, avoid them.\n\n` +
+    `If the repo has an AGENTS.md file give it precedence over README.md for developer documentation.\n\n`
 )
 
 
