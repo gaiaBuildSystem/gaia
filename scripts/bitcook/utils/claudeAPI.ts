2ea71bf4a9aa96f8122aa37007eac1a345066418
Matheus Castello <matheus@castello.eng.br>
2026-08-28 10:00:37 -0300

    scripts: mimir: Fix debug api url being in production
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/bitcook/utils/claudeAPI.ts
+++ scripts/bitcook/utils/claudeAPI.ts
@@ -43,8 +43,8 @@ export class ClaudeAPIClient {
     private _history: ChatTurn[] = []
 
     constructor (
-        // baseUrl: string = process.env.MIMIR_API_URL || "https://phobos.dev.br:8000"
-        baseUrl: string = process.env.MIMIR_API_URL || "http://localhost:8000"
+        baseUrl: string = process.env.MIMIR_API_URL || "https://phobos.dev.br:8000"
+        // baseUrl: string = process.env.MIMIR_API_URL || "http://localhost:8000"
     ) {
         this.baseUrl = baseUrl
     }
